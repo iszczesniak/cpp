c19cf0031d8e205e690030f3bd9bdc02e691e30c
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-10-03 12:40:18 +0200

    categories

--- categories/increment2.cc
+++ categories/increment2.cc
@@ -2,5 +2,5 @@ int main()
 {
   int x = 1;
   ++++x; // OK
-  x++++; // Error x++ is an rvalue, and <expr>++ wants an lvalue.
+  // x++++; // Error x++ is an rvalue, and <expr>++ wants an lvalue.
 }
