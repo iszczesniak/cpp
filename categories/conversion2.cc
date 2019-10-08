53e533c4e392d9f5d4ee87b91b3bb22394a592d4
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-10-08 13:21:35 +0200

    categories

--- categories/conversion2.cc
+++ categories/conversion2.cc
@@ -1,6 +1,10 @@
 int main()
 {
+  // The dereference operator requires an rvalue.
+  *((int *)0); // OK: "(int *)0" is a null pointer of type "int *".
+  *nullptr; // OK: nullptr is an rvalue.
+
   int x = 1;
   int *p = &x;
-  *p;
+  *p; // OK: "p" is an lvalue, but converted to an rvalue.
 }
--- categories/dereference.cc
+++
@@ -1,4 +0,0 @@
-int main()
-{
-  int &l = *(int *)0;
-}
