0c43ef6f142076053d8bfaf98561871af9c6e1f8
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-10-17 08:38:57 +0200

    sigsegv

--- memory/sigsegv.cc
+++ memory/sigsegv.cc
@@ -5,5 +5,9 @@ using namespace std;
 int main()
 {
   // "Hello!" is a string literal in the read-only memory.
-  *((char *)"Hello!") = 'Y';
+  *static_cast<char *>("Hello!") = 'Y';
+
+  // A string literal is of type "const char *", and that's why we had
+  // to static-cast it to "char *".  This would not compile:
+  // *"Hello!" = 'Y';
 }
