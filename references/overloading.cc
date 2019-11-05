04315e9f035126a844a4dd827f660f94df09db92
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-05 09:50:25 +0100

    references

--- references/overloading.cc
+++ references/overloading.cc
@@ -2,24 +2,29 @@
 
 using namespace std;
 
-void
-foo(int &)
-{
-  cout << "int &" << endl;
-}
+// Uncomment, and analyze the difference in overload resolution.
+//
+// // Overload #1
+// void
+// foo(int &)
+// {
+//   cout << "int &" << endl;
+// }
 
+// Overload #2
 void
 foo(const int &)
 {
   cout << "const int &" << endl;
 }
 
-// Odkomentować i sprawdzić, kiedy ta funkcja została wybrana.
+// Uncomment, and analyze the difference in overload resolution.
 //
+// // Overload #3
 // void
 // foo(int &&)
 // {
-//  cout << "int &&" << endl;
+//   cout << "int &&" << endl;
 // }
 
 int
@@ -27,15 +32,15 @@ main()
 {
   int x = 1;
   int &rx = x;
-  foo(x);
-  foo(rx);
+  foo(x); // Overload #1 if available, overload #2 otherwise.
+  foo(rx); // Overload #1 if available, overload #2 otherwise.
 
   const int y = 2;
   const int &ry = y;
-  foo(y);
-  foo(ry);
+  foo(y); // Always verload #2, an error otherwise.
+  foo(ry); // Always verload #2, an error otherwise.
 
   int &&rz = 3;
-  foo(3);
-  foo(rz);
+  foo(3); // Overload #3 if available, overload #2 otherwise.
+  foo(rz); // Overload #3 if available, overload #1 otherwise.
 }
