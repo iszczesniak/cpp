e82791d1ff9323f66ab71e23f42ea9a0a5b4a271
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-05 13:39:36 +0100

    rref

--- references/rref.cc
+++ references/rref.cc
@@ -18,5 +18,8 @@ int main()
   int &&x = 1;
 
   // Error: x is an lvalue, and an rvalue reference can't bind to it.
+  // x is an lvalue, because it has a name, even though it's of the
+  // rvalue reference type.
+
   // int &&z = x;
 }
