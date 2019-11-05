eacfba0657560acff5117b6e93c70449c1876daa
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-05 08:44:12 +0100

    move

--- references/move.cc
+++ references/move.cc
@@ -5,6 +5,9 @@ class A {};
 int main()
 {
   A a;
+  // How come an lvalue is not converted to an rvalue with the
+  // standard conversion?  This rule does not apply to reference
+  // initialization.
   A &&r1 = static_cast<A &&>(a);
   A &&r2 = std::move(a);
 }
