7cc1c7749610783cced29cd84223b923cbc9bbad
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-05 12:50:34 +0100

    references

--- references/quasi.cc
+++ references/quasi.cc
@@ -3,8 +3,13 @@
 #include <tuple>
 #include <vector>
 
+// This example demonstrates that std::pair and std::tuple can have
+// elements of a reference type.
 int main()
 {
+  // Error: the pair elements of reference type must be initialized.
+  // std::pair<int &, int &> p2;
+
   int x = 1;
 
   std::pair<int &, int &> p(x, x);
@@ -12,9 +17,7 @@ int main()
 
   std::cout << "x = " << x << std::endl;
 
-  // BŁĄD: elementy pary muszą być zainicjalizowane.
-  // std::pair<int &, int &> p2;
-
+  // The tuple elements of reference type must be initialized.
   std::tuple<int &, int &, int &> t(x, x, x);
   std::get<2>(t) = 3;
 
@@ -24,4 +27,5 @@ int main()
   v[1].second = 4;
 
   std::cout << "v[0].first = " << v[0].first << std::endl;
+  std::cout << "x = " << x << std::endl;
 }
