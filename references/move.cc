b9a054e95076c5fe4c2ed52bfe1925cd92322c6a
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-07 13:17:56 +0100

    refs

--- references/move.cc
+++ references/move.cc
@@ -5,9 +5,11 @@ class A {};
 int main()
 {
   A a;
+
   // How come an lvalue is not converted to an rvalue with the
-  // standard conversion?  This rule does not apply to reference
-  // initialization.
+  // standard conversion?  Because this rule does not apply to
+  // reference initialization.  The standard says that an rvalue
+  // reference must be initialized with an rvalue.
   A &&r1 = static_cast<A &&>(a);
   A &&r2 = std::move(a);
 }
