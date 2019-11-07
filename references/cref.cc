c2feef69758b64228f5a9ac2b98ea16811bcf521
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-07 10:57:50 +0100

    refs

--- references/cref.cc
+++ references/cref.cc
@@ -6,6 +6,7 @@ using namespace std;
 int
 foo(const string &)
 {
+  return 0;
 }
 
 int
@@ -16,8 +17,20 @@ main()
   // initializing expression `x` is automatically augmented with the
   // const qualifier.
   const int &l1 = x;
+
+  // Error: an lvalue reference cannot bind to an rvalue.
+  // int &l2 = 1;
   // The const reference binds to an rvalue.
-  const int &l2 = 1;
+  const int &l3 = 1;
+
+  // Expression "foo()" is an rvalue, because the function returns the
+  // result by value.
+  const int &c = foo("Hello!");
+  // Error: we cannot take the address of the returned value.
+  // const int *p = &foo("Hello!");
+  // But we can take the address to the temporary to which reference c
+  // is bound.  The reference extends the lifetime of the temporary.
+  const int *p = &c;
   
   string s;
   // The function parameter reference binds to an lvalue.
