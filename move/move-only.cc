15a9d46d8abdc176d1ea31ec7fcc78e479065291
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-11-22 06:45:39 +0100

    move

--- move/move-only.cc
+++ move/move-only.cc
@@ -1,6 +1,9 @@
 #include <utility>
 
-// A move-only type.
+// A move-only type.  We do not have to explicitely delete the copy
+// constructor, and the move assignment operator, because they will be
+// implicitely deleted, since the move constructor and the move
+// assignment operator are explicitely defaulted.
 struct A
 {
   A() = default;
@@ -12,6 +15,8 @@ int
 main()
 {
   A a;
-  // A b(a); // Error: we cannot copy.
+  // A b(a); // Error: we cannot copy initialize.
   A b(std::move(a));
+  // b = a; // Error: we cannot copy assign.
+  b = std::move(a);
 }
