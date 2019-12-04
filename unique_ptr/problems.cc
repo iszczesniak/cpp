42437961e0bb11841bb01df9174f114e25789e9b
Irek Szcześniak <irek.szczesniak@gmail.com>
2019-12-04 18:33:02 +0100

    problems

--- unique_ptr/problems.cc
+++ unique_ptr/problems.cc
@@ -1,16 +1,27 @@
 #include <iostream>
 
+// Who should destroy the allocated data?  Should the data be
+// destroyed by the foo function?
+
 void
 foo(int *p)
 {
   // Do sth with p.
 
-  // Should I destroy p?  I guess.
+  // We should have destroyed the data with the array version of
+  // delete (i.e., delete [] p), because it was allocated with the
+  // array version of new.
   delete p;
 }
 
 int
 main()
 {
-  int *p = new int(1);
+  // The problem is brewing: we use a pointer to integer to point to
+  // the begining of an array of integers.
+  int *p = new int[5];
+  foo(p);
+
+  // This is the second delete.
+  delete [] p;
 }
