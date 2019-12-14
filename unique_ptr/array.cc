ed1ac5ea50dd0c788c43f8ba61ec04f60e9c81f5
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2019-12-14 09:36:23 +0100

    array

--- unique_ptr/array.cc
+++ unique_ptr/array.cc
@@ -6,6 +6,6 @@ using namespace std;
 int
 main()
 {
-  unique_ptr<array<int, 5>> up(new array<int, 5>);
-  auto up = make_unique<array<int, 5>>();
+  unique_ptr<array<int, 5>> up1(new array<int, 5>);
+  auto up2 = make_unique<array<int, 5>>();
 }
