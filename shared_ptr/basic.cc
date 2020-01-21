d9d1a0817fd4559bea070be357c9748e6e4c2d42
Irek Szcześniak <irek.szczesniak@gmail.com>
2020-01-21 13:24:36 +0100

    basic

--- shared_ptr/basic.cc
+++ shared_ptr/basic.cc
@@ -1,6 +1,8 @@
+#include <cassert>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <utility>
 
 using namespace std;
 
@@ -15,23 +17,53 @@ struct A
 
   ~A()
   {
-    cout << "dtor:" << m_text << endl;
+    cout << "dtor: " << m_text << endl;
   }
 };
 
 int main (void)
 {
   // sp takes the ownership.
-  shared_ptr sp(new A("A1"));
-  sp.reset(new A("A2"));
-}
+  shared_ptr<A> sp(new A("A1"));
+  assert(sp);
 
-  \begin{itemize}
-  \item Oddanie pod zarządzanie: \code{shared_ptr<A> sp(new A());}
-  \item Oddanie pod zarządzanie: \code{sp.reset(new A());}
-  \item Kopiowanie obiektów: \code{shared_ptr<A> sp2(sp);}
-  \item Kopiowanie obiektów: \code{sp2 = sp;}
-  \item Przenoszenie obiektów: \code{shared_ptr<A> sp2(move(sp));}
-  \item Przenoszenie obiektów: \code{sp2 = move(sp);}
-  \item Niszczenie: tego nie robimy, to odbywa się automatycznie.
-  \end{itemize}
+  // We make sp manage a new object.  A1 is destroyed.
+  sp.reset(new A("A2"));
+
+  {
+    // We copy-initialize the ownership.
+    shared_ptr<A> sp2(sp);
+    assert(sp);
+    assert(sp2);
+
+    shared_ptr<A> sp3;
+    // We copy-assign the ownership.
+    sp3 = sp2;
+    assert(sp2);
+    assert(sp3);
+
+    // Even though sp2 and sp3 go out of scope, A2 will not be
+    // destroyed, because it's still being managed by sp.
+  }
+
+  {
+    // We move-initialize the ownership.
+    shared_ptr<A> sp2(move(sp));
+    assert(!sp);
+    assert(sp2);
+
+    shared_ptr<A> sp3;
+    // We move-assign the ownership.
+    sp3 = move(sp2);
+    assert(!sp2);
+    assert(sp3);
+
+    // A2 is destroyed, because sp3 goes out of scope.
+  }
+
+  // We can't release the managed data from being managed, as we are
+  // able to do with unique_ptr, because we can't preempt (strip)
+  // other shared_ptr objects of their ownership.
+
+  // sp.release();
+}
