fc585e22442b59b649195e34260068bb7d95d775
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2020-03-23 17:44:28 +0100

    motivation

--- call_pl/motivation2.cc
+++ call_pl/motivation2.cc
@@ -9,13 +9,19 @@ struct A
   A(int id): id(id)
   {
   }
+
+  bool
+  operator < (const A &a) const
+  {
+    return this->id < a.id;
+  }
 };
 
 int
 main()
 {
-  vector v{3, 1, 2};
+  vector v{A(3), A(1), A(2)};
   sort(v.begin(), v.end());
   for(const auto &e: v)
-    cout << e << endl;
+    cout << e.id << endl;
 }
