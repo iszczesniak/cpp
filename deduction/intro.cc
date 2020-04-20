d5b8aaeb4fd16e9dca52ff55e378f388adef0624
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2020-04-20 17:32:17 +0200

    intro

--- deduction/intro.cc
+++ deduction/intro.cc
@@ -2,10 +2,11 @@
 
 using namespace std;
 
-template <typename T>
+template <typename T = int>
 void
-print(T t)
+print(T t = {})
 {
+  cout << __PRETTY_FUNCTION__ << endl;
   cout << t << endl;
 }
 
@@ -22,4 +23,9 @@ main()
   print<int>(1.2);          // T = int
   print<double>(1);         // T = double
   print<string>("Hello!");  // T = string
+  print<double>();          // T = double
+
+  // We use the default template argument (int), and a default value
+  // for a call argument ({}, which is 0 for int).
+  print();
 }
