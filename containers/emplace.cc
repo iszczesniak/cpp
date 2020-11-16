98ff7870b6fd0de67425af14cf907b0e72d9d7cb
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2020-11-16 10:39:26 +0100

    emplace

--- containers/emplace.cc
+++ containers/emplace.cc
@@ -37,51 +37,70 @@ struct A
     cout << "move-assignment: " << m_id << '\n';
     return *this;
   }
-
-  bool
-  operator < (const A &a) const
-  {
-    return m_id < a.m_id;
-  }
 };
 
 int main()
 {
-  vector<A> v;
-  // This becomes the first element in the vector.
-  cout << "Checkpoint 1\n";
-  v.emplace_back("V1");
-  cout << "Checkpoint 2\n";
-  v.emplace(v.begin(), "V2");
-  cout << "Checkpoint 3\n";
-  v.emplace(v.end(), "V3");
+  cout << "vector: -----------------------------------------\n";
 
-  for(const auto &e: v)
-    cout << e.m_id << endl;
+  {
+    vector<A> v;
+
+    // Uncommend the line below to prevent vector reallocation.
+    // v.reserve(3);
+
+    auto i1 = v.begin();
+
+    // This becomes the first element in the vector.
+    cout << "Checkpoint V1\n";
+    v.emplace_back("V1");
+    cout << "Checkpoint V2\n";
+    v.emplace(v.begin(), "V2");
+    cout << "Checkpoint V3\n";
+    v.emplace(v.end(), "V3");
+
+    cout << "The vector elements are:\n";
+    for(const auto &e: v)
+      cout << e.m_id << endl;
+
+    cout << "The vector was relocated: " << boolalpha
+         << (i1 != v.begin()) << endl;
+  }
   
-  cout << "-------------------------------------------------\n";
+  cout << "list: -------------------------------------------\n";
 
-  // list<A> l;
-  // l.emplace_front("L1");
-  // l.emplace_back("L2");
+  {
+    list<A> l;
+    // We can emplace at the front, and at the back, because the list
+    // is doubly-linked.
+    l.emplace_front("L1");
+    l.emplace_back("L2");
+    l.emplace(++(l.begin()), "L3");
 
-  // cout << "-------------------------------------------------\n";
+    cout << "The list elements are:\n";
+    for(const auto &e: l)
+      cout << e.m_id << endl;
+  }
+  
+  cout << "forward_list: -----------------------------------\n";
 
-  // forward_list<A> f;
-  // f.emplace_front("L1");
-  // f.emplace_after(f.begin(), "L2");
+  {
+    forward_list<A> f;
+    f.emplace_front("L1");
+    // We can emplace after an element, but not before, because it's a
+    // singly-linked list.
+    f.emplace_after(f.begin(), "L2");
 
-  // // We can't emplace at the back, because we don't have an iterator
-  // // to the preceeding element in the list.
+    // We can't emplace at the back, because we don't have an iterator
+    // to the preceeding element in the list.
 
-  // // f.emplace_back("L3");
+    // f.emplace_back("L3");
 
-  // cout << "-------------------------------------------------\n";
+    cout << "The forward_list elements are:\n";
+    for(const auto &e: f)
+      cout << e.m_id << endl;
 
-  // set<A> s;
-  // s.emplace("S1");
-
-  // cout << "-------------------------------------------------\n";
+  }
 
   return 0;
 }
