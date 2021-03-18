d5426b88b3026422519227a3d33ee978764dae9a
Irek Szcześniak <irek.szczesniak@gmail.com>
2021-03-18 07:07:40 +0100

    struct

---
+++ specialization/struct.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+
+// Definition of a primary template.
+template <typename T>
+struct A
+{
+  void
+  foo()
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+  }
+};
+
+// A complete specialization.
+template<>
+struct A<double>
+{
+  void
+  goo()
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+  }  
+};
+
+int
+main()
+{
+  A<int> i;
+  i.foo();
+  A<float>().foo();
+
+  // Error: "foo" is in the primary template, which we've overwritten.
+  // A<double>().foo();
+
+  // But we can call "goo" alright.
+  A<double>().goo();
+}
