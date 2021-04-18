185b82ddb8ac07fff11104a2529792dd024eda06
Irek Szcześniak <irek.szczesniak@gmail.com>
2021-04-18 09:42:52 +0200

    function

--- callable/function.cc
+++ callable/function.cc
@@ -10,14 +10,24 @@ foo(const int &a, const int &b)
 int
 main()
 {
-  // I don't like the pointer-to-function C syntax.
-  bool (*f2a)(const int &, const int &) = foo;
-  bool (*f2b)(const int &, const int &) = &foo;
-  f2a(10, 20);
-  (*f2a)(10, 20);
-  f2b(10, 20);
-  (*f2b)(10, 20);
+  // I don't like C syntax of the pointer to a function.
+  bool (*f1a)(const int &, const int &) = foo;
+  bool (*f1b)(const int &, const int &) = &foo;
+  f1a(10, 20);
+  (*f1a)(10, 20);
+  f1b(10, 20);
+  (*f1b)(10, 20);
 
+  // A reference to a function.
+  bool (&f2a)(const int &, const int &) = foo;
+  // A reference cannot be initialized with a pointer.
+  // bool (&f2b)(const int &, const int &) = &foo;
+  f2a(10, 20);
+  // The following is wierd, but it compiles. f1a is an alias for foo.
+  // So f1a is replaced with foo, and foo decays into a pointer, which
+  // we dereference to get a function to call.
+  (*f2a)(10, 20);
+  
   // That's better.
   using foo_type = bool(const int &a, const int &b);
   foo_type *f3a = foo;
