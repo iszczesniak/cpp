8d9af3d1562d51dc9cb36541d023b151ce086a79
Irek Szcześniak <irek.szczesniak@gmail.com>
2021-04-21 22:31:21 +0200

    time

--- callable2/time_it.cc
+++ callable2/time_it.cc
@@ -14,17 +14,16 @@ time_it(F f, Args &&... args)
   std::invoke(f, std::forward<Args>(args)...);
 
   auto t1 = std::chrono::system_clock::now();
-  std::chrono::duration<double> dt = t1 - t0;
+  std::chrono::nanoseconds dt = t1 - t0;
 
-  return dt;
+  return dt.count();
 }
 
 template <typename T>
-T
+void
 foo(T x)
 {
-  x += 1;
-  return x;
+  cout << __PRETTY_FUNCTION__ << " took ";
 }
 
 struct A
@@ -37,12 +36,12 @@ struct A
 
   void foo(int x)
   {
-    cout << __PRETTY_FUNCTION__ << ": " << m_name << endl;
+    cout << __PRETTY_FUNCTION__ << " for " << m_name << " took ";
   }
 
   void goo(int x)
   {
-    cout << __PRETTY_FUNCTION__ << ": " << m_name << endl;
+    cout << __PRETTY_FUNCTION__ << " for " << m_name << " took ";
   }
 };
 
@@ -54,12 +53,14 @@ main()
   void (A::* p1)(int) = &A::foo;
   auto p2 = &A::goo;
 
-  (a.*p1)(1);
-  (b.*p1)(1);
+  //  (a.*p1)(1);
+  //  (b.*p1)(1);
 
-  (a.*p2)(1);
-  (b.*p2)(1);
+  //  (a.*p2)(1);
+  //  (b.*p2)(1);
 
-  time_it(foo<int>, 1);
-  time_it(p1, a, 1);
+  cout << time_it(foo<int>, 1) << " ns\n";
+  cout << time_it(p1, a, 1) << " ns\n";
+  cout << time_it(foo<int>, 1) << " ns\n";
+  cout << time_it(p1, a, 1) << " ns\n";
 }
