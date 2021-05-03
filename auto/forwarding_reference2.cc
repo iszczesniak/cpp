56ffc8b2625492a1ca42959c2ed3292c6fc7065e
Irek Szcześniak <irek.szczesniak@gmail.com>
2021-05-03 19:00:21 +0200

    auto

--- auto/forwarding_reference2.cc
+++ auto/forwarding_reference2.cc
@@ -7,13 +7,16 @@ main()
   std::vector<int> vi = {1};
   std::set<int> s = {1};
 
-  // We can initialize a non-const reference to a vector element.
+  // We can initialize a non-const reference to a vector element,
+  // because the dereference operator returns a non-const lvalue
+  // reference to a vector element.
   int &a = *vi.begin();
 
   // We cannot initialize a non-const reference to a set element.
   // int &b = *s.begin();
 
-  // The reference has to be const.
+  // The reference has to be const, because the dereference operator
+  // returns a const lvalue reference to a set element.
   const int &c = *s.begin();
 
   // It's best to let the compiler figure out the right type.  As part
@@ -22,8 +25,13 @@ main()
   auto &e = *s.begin();
 
   std::vector<bool> vb = {true};
+  // We cannot initialize an lvalue reference, because the
+  // initializing expression is an rvalue: the dereference operator
+  // returns a temporary proxy object that can convert to a bool.
   // auto &f = *vb.begin();
-  auto &&f = *vb.begin();
+
+  // We have to use an rvalue reference.
+  bool &&f = *vb.begin();
 
   // So it's best to use the forwarding reference:
   auto &&g = *vi.begin();
