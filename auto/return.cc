ce8f3eed9b060748fd05f6849e54d0875798d4c8
Irek Szcześniak <irek.szczesniak@gmail.com>
2021-05-04 20:40:33 +0200

    return

--- auto/return.cc
+++ auto/return.cc
@@ -1,10 +1,26 @@
-auto
-foo()
+auto f1()
 {
   return 0;
 }
 
+auto f2()
+{
+  static int i = 0;
+  return i;
+}
+
+decltype(auto) f3()
+{
+  static int i = 0;
+  return i;
+}
+
 int main()
 {
-  foo();
+  f1();
+
+  // This will not compile, because f2 returns int.
+  // ++f2();
+  
+  ++(f3());
 }
