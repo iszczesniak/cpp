5874adc30a7d22916a2d59669a1176091a495fca
Irek Szcześniak <irek.szczesniak@gmail.com>
2021-05-06 21:31:14 +0200

    auto

---
+++ auto/for_auto.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int
+main()
+{
+  map<int, string> m = {{1, "Hello"}, {2, "World"}};
+  map<int, const string &> mr;
+
+  for(const pair<int, string> &e: m)
+    mr.insert(make_pair(e.first, e.second));
+
+  for(auto &e: mr)
+    cout << e.first << ", " << e.second << endl;
+}
