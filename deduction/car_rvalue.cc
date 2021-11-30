f63f14c59c4de327092ccef791f3980ef6a10ab7
Irek Szcześniak <irek.szczesniak@gmail.com>
2021-11-30 13:05:34 +0100

    carr

--- deduction/car_rvalue.cc
+++ deduction/car_rvalue.cc
@@ -1,25 +1,27 @@
 #include <initializer_list>
 #include <iostream>
 
+using namespace std;
+
 template<typename T>
 void foo(T &)
 {
   cout << "Overload #1: non-const lvalue reference\n";
-  cout << __PRETTY_FUNCTION__ << endl;
+  cout << __PRETTY_FUNCTION__ << "\n\n";
 }
 
 template<typename T>
 void foo(const T &)
 {
-  cout << "Overload #1: const reference\n";
-  cout << __PRETTY_FUNCTION__ << endl;
+  cout << "Overload #2: const reference\n";
+  cout << __PRETTY_FUNCTION__ << "\n\n";
 }
 
 template<typename T>
-void foo(const T &)
+void foo(initializer_list<T> &)
 {
   cout << "Overload #1: const reference\n";
-  cout << __PRETTY_FUNCTION__ << endl;
+  cout << __PRETTY_FUNCTION__ << "\n\n";
 }
 
 int main()
@@ -29,5 +31,5 @@ int main()
   const int b[] = {1, 2, 3};
   foo(b); // Overload #2
   
-  foo({1, 2, 3});
+  //  foo({1, 2, 3});
 }
