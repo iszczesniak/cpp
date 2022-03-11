ab79b0b1d3b20da99650e61c19ff353334b188a1
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-03-11 12:03:19 +0100

    string

---
+++ categories/string.cc
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string
+operator++(string &s, int)
+{
+  string tmp = s;
+  next_permutation(s.begin(), s.end());
+  return tmp;
+}
+
+int main()
+{
+  cout << "Permutations for abc:" << endl;
+  for(string i = "abc"; i++ != "cba";)
+    cout << i << endl;
+}
