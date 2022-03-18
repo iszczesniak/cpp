83c5900c5f4264bc8ef950184c5c56ae0dc7331a
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-03-18 12:50:50 +0100

    elide

---
+++ move/elide.cc
@@ -0,0 +1,30 @@
+#include<utility>
+#include<iostream>
+
+struct A
+{
+  A() = default;
+
+  // Constructors not elided, so cannot delete them.
+  // A(const A &) = delete;
+  // A(A &&) = delete;
+};
+
+struct B
+{
+  A m_a;
+
+  // Parameter a has to be copied or moved to m_a.  We move.
+  // Constructor elision (for copying or moving from a to m_a) cannot
+  // take place, because parameter a was already created by the
+  // caller, and the constructor can only copy or move.
+  B(A a): m_a(std::move(a))
+  {
+  }
+};
+
+int
+main()
+{
+  B(A{});
+}
