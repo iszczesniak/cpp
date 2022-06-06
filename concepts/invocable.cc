83809db53a95cb420ecb836db9e5daf107317b7d
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-06-06 22:23:10 +0200

    concepts

--- concepts/invocable.cc
+++ concepts/invocable.cc
@@ -7,10 +7,21 @@ using namespace std;
 template <typename ... Args>
 using EmptyCallable = decltype([](Args ...){});
 
+template <typename T = EmptyCallable<int, string>>
+void f1(T t = {}) requires invocable<T, int, string>
+{
+  t(2, "Hello!");
+}
+
 template <invocable<int, string> T = EmptyCallable<int, string>>
-void f(T t = {})
+void f2(T t = {})
 {
   t(1, "Hello!");
+}
+
+void f3(invocable<int, string> auto t = EmptyCallable<int, string>{})
+{
+  t(3, "Hello!");
 }
 
 void
@@ -21,6 +32,18 @@ foo(int i, string s)
 
 int main()
 {
-  f();
-  f(foo);
+  f1();
+  f1(foo);
+  f2();
+  f2(foo);
+
+  // The following should compile, I reckon, because the variable t
+  // below (which is just like the parameter of function f3) compiles
+  // fine.  Yet, the function call doesn't with GCC 12.1.0.
+
+  // f3();
+
+  invocable<int, string> auto t = EmptyCallable<int, string>{};
+
+  f3(foo);
 }
