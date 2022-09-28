9dae98a2b1f8ad4e30bf82b4bfa8f028a356a3d1
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2022-09-28 10:29:59 +0200

    partial

--- overloading/partial.cc
+++ overloading/partial.cc
@@ -1,11 +1,13 @@
 #include <iostream>
 
+// Template #1
 template <typename T>
 void foo(T t1, T t2)
 {
   std::cout << "Template with one parameter.\n";
 }
 
+// Template #2
 template <typename T1, typename T2>
 void foo(T1 t1, T2 t2)
 {
@@ -14,5 +16,30 @@ void foo(T1 t1, T2 t2)
 
 int main()
 {
+  // Template #1 is transformed into this function type:
+  //
+  // void foo(F, F)
+
+  // To get F, [temp.func.order] says "synthesize a unique type"
+  // without further explanation.  I guess that:
+  //
+  // * "to synthesize" implies taking into account the function call,
+  //
+  // * "unique" means that F is no longer a template parameter.
+  //
+  // For the following function call, I guess the first template is
+  // intransformed into this function type:
+  //
+  // void foo(int, int) // Function type #1
+  //
+  // The above type is the function type transformed from the argument
+  // template.
+  //
+  // Template #2 is called the parameter template, because for that
+  // template we try to deduce the arguments based on the function
+  // type #1:
+  //
+  // For template #2, T1 = int, T2 = int.  Success.
+
   foo(1, 1);
 }
