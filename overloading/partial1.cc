b00173bde4cd585deafa914ac9a20898f819cb84
Irek Szcześniak <irek.szczesniak@gmail.com>
2022-09-30 19:33:14 +0200

    overloads

---
+++ overloading/partial1.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+template <typename T>
+void foo(T &)
+{
+  cout << __PRETTY_FUNCTION__ << endl;
+}
+
+template <typename T>
+void foo(const T &)
+{
+  cout << __PRETTY_FUNCTION__ << endl;
+}
+
+int main()
+{
+  int x = 1;
+  foo(x);
+
+  const cx = 1;
+  foo(cx);  
+}
