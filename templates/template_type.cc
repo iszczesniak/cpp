97ef320fa509b6256e04e4143219daf8a92ca5f7
Irek Szcześniak <irek.szczesniak@gmail.com>
2023-03-03 21:19:55 +0100

    templates

--- templates/template_type.cc
+++ templates/template_type.cc
@@ -1,4 +1,3 @@
-#include <forward_list>
 #include <iostream>
 #include <list>
 #include <vector>
@@ -14,30 +13,16 @@ foo(T t)
   c.push_back(t);
 }
 
-// Type my_list has only one template parameter.  We pass only one
-// parameter to std::list, and use the default values for whatever
-// remaining parameters there are for std::list.
-template <typename T>
-using my_list = std::list<T>;
-
-// using my_vector
-template <typename T>
-using my_vector = std::vector<T>;
-
-// using my_forward_list
-template <typename T>
-using my_forward_list = std::forward_list<T>;
-
 int
 main()
 {
-  foo<my_list>(1);
-  foo<my_list>("Hello");
-  foo<my_vector>(.1);
-  foo<my_vector>("World");
+  foo<list>(1);
+  foo<list>("Hello");
+  foo<vector>(.1);
+  foo<vector>("World");
 
   // This is cool: pointer to an instantiated function template.  We
   // instantiate the function right here, because we point to it.
-  void (*fp)(double) = foo<my_list, double>;
+  void (*fp)(double) = foo<list, double>;
   fp(1);
 }
