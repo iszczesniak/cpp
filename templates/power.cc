66c665ad2b5eb45ad4a359465605760ba88de15b
Irek Szcześniak <irek.szczesniak@gmail.com>
2023-03-03 10:14:43 +0100

    power

--- templates/power.cc
+++ templates/power.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+
 template <unsigned I, typename T>
 T
 power(T t)
@@ -11,5 +13,14 @@ power(T t)
 int
 main()
 {
-  
+  // The value calculated at compile-time, nothing at run-time.
+  std::cout << power<2>(.2) << std::endl;
+
+  // We use the volatile qualifier to force evaluation at run-time.
+  volative int d = 3;
+  // The assemby code uses the value of d without any calculation.
+  std::cout << power<1>(d) << std::endl;
+
+  // The assemby code just multiplies d by itself.
+  std::cout << power<2>(d) << std::endl;
 }
