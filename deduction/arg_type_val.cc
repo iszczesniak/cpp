5c3af34866714e8427b196a8e8c189c4874842f8
Irek Szcześniak <irek.szczesniak@gmail.com>
2023-03-07 14:28:06 +0100

    deduction

--- deduction/arg_type_val.cc
+++ deduction/arg_type_val.cc
@@ -50,4 +50,44 @@ main()
   foo4(x);
   foo4(y);
   foo4(z);
+
+  int &rx = x;
+  const int &ry = y;
+  const volatile int &rz = z;
+
+  foo1(rx);
+  foo1(ry);
+  foo1(rz);
+
+  foo2(rx);
+  foo2(ry);
+  foo2(rz);
+
+  foo3(rx);
+  foo3(ry);
+  foo3(rz);
+
+  foo4(rx);
+  foo4(ry);
+  foo4(rz);
+
+  int * const volatile cvpx = &x;
+  const int * const volatile cvpy = &y;
+  const volatile int * const volatile cvpz = &z;
+
+  foo1(cvpx);
+  foo1(cvpy);
+  foo1(cvpz);
+
+  foo2(cvpx);
+  foo2(cvpy);
+  foo2(cvpz);
+
+  foo3(cvpx);
+  foo3(cvpy);
+  foo3(cvpz);
+
+  foo4(cvpx);
+  foo4(cvpy);
+  foo4(cvpz);
 }
