7a3ac5eb0c0f62be23837019af9a710a4c7c5251
Irek Szcześniak <irek.szczesniak@gmail.com>
2023-03-14 11:34:33 +0100

    bubble

--- templates/bubble.cc
+++ templates/bubble.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iterator>
-#include <forward_list>
+#include <list>
+#include <vector>
 
 using namespace std;
 
@@ -8,10 +9,11 @@ using namespace std;
 template <typename I, typename F>
 void bubble(I i1, I i2, F f)
 {
-  while(i1 != i2)
-    for(auto j = ++i1; j != i2; ++j)
-      if (f(*i1, *j))
-        std::swap(*i1, *j);  
+  if (i1 != i2)
+    while(i1 != --i2)
+      for(auto i = i1; i != i2; ++i)
+        if (!f(*i, *(i+1)))
+          std::swap(*i, *(i+1));  
 }
 
 bool cmp(const int &a, const int &b)
@@ -27,10 +29,15 @@ main()
   for(const auto &e: t)
     cout << e << '\n';
 
+  vector v = {1, 5, 3, 2};
+  bubble(begin(v), end(v), cmp);
+  for(const auto &e: v)
+    cout << e << '\n';
+
   cout << "*****************************\n";
   
-  forward_list fl = {3, 4, 2, 0};
-  bubble(begin(fl), end(fl), cmp);
-  for(const auto &e: fl)
-    cout << e << '\n';
+  // list fl = {3, 4, 2, 0};
+  // bubble(begin(fl), end(fl), cmp);
+  // for(const auto &e: fl)
+  //   cout << e << '\n';
 }
