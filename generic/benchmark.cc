a0f5409574fe753973235b80459fbaa3f2b17b59
Irek Szcześniak <irek.szczesniak@gmail.com>
2023-03-27 07:52:05 +0200

    chrono

--- generic/benchmark.cc
+++ generic/benchmark.cc
@@ -6,8 +6,6 @@
 
 using namespace std;
 
-using tp_t = chrono::time_point<chrono::high_resolution_clock>;
-
 int main()
 {
   const int n = 100000000;
@@ -20,24 +18,23 @@ int main()
   v.reserve(n);
 
   {
-    tp_t t0 = std::chrono::system_clock::now();
+    auto t0 = std::chrono::system_clock::now();
 
     for(int i = 0; i < n; ++i)
       v.push_back(distr(gen));
 
-    tp_t t1 = std::chrono::system_clock::now();
+    auto t1 = std::chrono::system_clock::now();
     double dt = static_cast<chrono::duration<double>>(t1 - t0).count();
 
     cout << "generation: " << dt << " s\n";
   }
 
   {
-    using tp_t = chrono::time_point<chrono::high_resolution_clock>;
-    tp_t t0 = std::chrono::system_clock::now();
+    auto t0 = std::chrono::system_clock::now();
 
     sort(v.begin(), v.end());
 
-    tp_t t1 = std::chrono::system_clock::now();
+    auto t1 = std::chrono::system_clock::now();
     double dt = static_cast<chrono::duration<double>>(t1 - t0).count();
 
     cout << "sorting: " << dt << " s\n";
