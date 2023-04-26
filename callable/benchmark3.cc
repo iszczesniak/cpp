7f30404647abea1ba899266df0813aa1f2ada460
iszczesniak <irek.szczesniak@gmail.com>
2023-04-26 11:33:54 +0200

    callable

--- callable/benchmark3.cc
+++ callable/benchmark3.cc
@@ -26,14 +26,22 @@ int
 main()
 {
   bool flag = true;
-  cout << "Compare with a functor -----------------------------\n";
-  doit(priority_queue<student, vector<student>, fun>(fun(flag)));
-  cout << "Compare with a closure -----------------------------\n";
-
-  auto c = [flag](const student &a, const student &b)
+  auto c = [&flag](const student &a, const student &b)
   {
     return flag ? tie(a.m_year, a.m_name) < tie(b.m_year, b.m_name)
 		: tie(a.m_year, a.m_name) > tie(b.m_year, b.m_name);
   };
+
+  cout << "Compare with a functor -----------------------------\n";
+  doit(priority_queue<student, vector<student>, fun>(fun(flag)));
+  cout << "Compare with a closure -----------------------------\n";
+  doit(priority_queue<student, vector<student>, decltype(c)>(c));
+
+  cout << "Enter 0 or 1: ";
+  cin >> flag;
+
+  cout << "Compare with a functor -----------------------------\n";
+  doit(priority_queue<student, vector<student>, fun>(fun(flag)));
+  cout << "Compare with a closure -----------------------------\n";
   doit(priority_queue<student, vector<student>, decltype(c)>(c));
 }
