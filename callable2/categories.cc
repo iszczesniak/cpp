df32b28824239fbbe6e21f4283852c84da94fed3
iszczesniak <irek.szczesniak@gmail.com>
2023-05-16 08:47:21 +0200

    callable2

--- callable2/categories.cc
+++ callable2/categories.cc
@@ -7,6 +7,7 @@ template <typename C>
 void f(C &&c)
 {
   cout << __PRETTY_FUNCTION__ << '\n';
+  // Here we perfectly forward the callable.
   forward<C>(c)();
 }
 
@@ -29,12 +30,18 @@ struct callme
 
 int main()
 {
+  // Because function f accepts a callable by a forwarding reference,
+  // we can pass to it by reference both a function and a closure.
+
   // Expression g is an lvalue.
   f(g); // C = void (&)()
   // The lambda expression in an rvalue.
   f([]{});
 
+  // Forwarding the callable in function f is needed here.
   callme c;
+  // Function f should call the passed callable as an lvalue.
   f(c);
+  // Function f should call the passed callable as an rvalue.
   f(callme());
 }
