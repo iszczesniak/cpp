5d6d8f71de57d8bd0fecb5c3842a340e511fe029
Ireneusz Szcześniak <irek.szczesniak@gmail.com>
2023-05-18 14:25:52 +0200

    overloads

--- callable2/overloads.cc
+++ callable2/overloads.cc
@@ -11,18 +11,40 @@ void foo(int &&x)
   std::cout << "Rvalue overload\n";
 }
 
+struct A
+{
+  void foo() &
+  {
+  }
+
+  void foo() &&
+  {
+  }
+};
+
 int
 main()
 {
   int x;
+  A a;
 
   // Regular function call.
   foo(x);
   foo(1);
 
-  // std::apply(foo, x);
-  // std::apply(foo, 1);
-
   // std::invoke(foo, x);
   // std::invoke(foo, 1);
+
+  // std::apply(foo, std::forward_as_tuple(x));
+  // std::apply(foo, std::forward_as_tuple(1));
+
+  // Regular member function call.
+  a.foo();
+  A().foo();
+
+  // std::invoke(&A::foo, a);
+  // std::invoke(&A::foo, A());
+
+  // std::apply(&A::foo, std::forward_as_tuple(a));
+  // std::apply(&A::foo, std::forward_as_tuple(A()));
 }
