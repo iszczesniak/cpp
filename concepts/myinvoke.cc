3ce42fd27d1db0f5609649d8e5834f4a0e549b44
iszczesniak <irek.szczesniak@gmail.com>
2023-05-23 09:33:54 +0200

    myinvoke

--- concepts/myinvoke.cc
+++ concepts/myinvoke.cc
@@ -1,18 +1,45 @@
-#include <type_traits>
+#include <iostream>
 #include <utility>
+#include <type_traits>
 
-template <typename G, typename O, typename ... Args>
-requires std::is_member_function_pointer<G>
-void myinvoke(G &&g, O &&o, Args &&... args)
+template <typename F>
+concept member_pointer = std::is_member_pointer_v<F>;
+
+// A generic primary template overload.
+template <typename G, typename ... Args>
+void myinvoke(G &&g, Args &&... args)
 {
-  std::forward<O>(o).*std::forward<G>(g)()
-    (std::forward<Args>(args)...);
+  std::forward<G>(g)(std::forward<Args>(args)...);
+}
+
+// A more specialized primary template.
+template <member_pointer G, typename O, typename ... Args>
+void
+myinvoke(G &&g, O &&o, Args &&... args)
+{
+  (std::forward<O>(o).*std::forward<G>(g))(std::forward<Args>(args)...);
+}
+
+void foo()
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+void goo(int)
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
 struct A
 {
   void foo()
   {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
+  }
+
+  void goo(int)
+  {
+    std::cout << __PRETTY_FUNCTION__ << std::endl;
   }
 };
 
@@ -20,5 +47,10 @@ int
 main()
 {
   A a;
+
+  myinvoke(foo);
+  myinvoke(goo, 1);
+  myinvoke([]{});
   myinvoke(&A::foo, a);
+  myinvoke(&A::goo, a, 1);
 }
