cf9fe998a8fe5d4448a459d0ad6588c0b13e27d6
iszczesniak <irek.szczesniak@gmail.com>
2023-05-23 12:26:42 +0200

    callable

--- constraints/callable.cc
+++ constraints/callable.cc
@@ -1,5 +1,7 @@
 #include <utility>
 
+// Check whether we could call g with 1.  I don't know how to check
+// that the type of the first parameter of a callable is integral.
 template <typename G> 
 void f(G &&g) requires requires
 {std::forward<G>(g)(1);}
