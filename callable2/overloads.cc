27afacc90a5e24c06fe1fffe891a76c96077fb82
iszczesniak <irek.szczesniak@gmail.com>
2023-05-25 13:38:20 +0200

    callable2

--- callable2/overloads.cc
+++ callable2/overloads.cc
@@ -44,6 +44,9 @@ main()
   foo(x);
   foo(1);
 
+  // We don't want to use static_cast, it's not generic.
+  std::invoke(static_cast<void(*)(int &)>(foo), x);
+  std::invoke(static_cast<void(*)(int &&)>(foo), 1);
   // std::invoke(foo, x);
   // std::invoke(foo, 1);
 
