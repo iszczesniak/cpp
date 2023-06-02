1359bddc44f6c924e24c7b0c0e0f95189206b53a
iszczesniak <irek.szczesniak@gmail.com>
2023-06-02 09:54:19 +0200

    mysort

---
+++ concepts/mysort.cc
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iterator>
+#include <list>
+#include <string>
+#include <vector>
+
+template <typename C, typename I>
+concept mycallable =
+  std::invocable<C, std::iter_const_reference_t<I>,
+                 std::iter_const_reference_t<I>>;
+
+template <std::random_access_iterator I, mycallable<I> C>
+void
+mysort(I &&i1, I &&i2, C &&c)
+{
+  std::sort(i1, i2, c);
+}
+
+bool
+foo(const int &a, const int &b)
+{
+  return a < b;
+}
+
+bool
+goo(const std::string &a, const std::string &b)
+{
+  return a < b;
+}
+
+int
+main()
+{
+  std::vector v = {3, 1, 2};
+  mysort(v.begin(), v.end(), foo);
+  // mysort(v.begin(), v.end(), goo);
+
+  std::list l = {3, 1, 2};
+  // mysort(l.begin(), l.end(), foo);
+}
