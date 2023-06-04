8144d02e0da037ddf80021f89c78390e37fa2cff
Irek Szcześniak <irek.szczesniak@gmail.com>
2023-06-04 23:31:09 +0200

    variadic

--- variadic/print.cc
+++ variadic/print.cc
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <string>
+
+template <typename Arg1, typename ...Args>
+void
+print(const Arg1 &arg1, const Args &...args)
+{
+  std::cout << arg1;
+
+  if constexpr(sizeof...(args))
+    print(args...);
+}
 
 int
 main()
 {
+  print("Hello", ' ', std::string("World x "), 100, '\n');
 }
