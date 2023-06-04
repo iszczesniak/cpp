0ff51c9f924605766ca2f1391472797d920cedee
iszczesniak <irek.szczesniak@gmail.com>
2023-06-04 11:10:28 +0200

    variadic

---
+++ variadic/intro.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+template <unsigned ...N>
+void foo()
+{
+  cout << __PRETTY_FUNCTION__ << endl;
+  cout << sizeof...(N) << endl;
+}
+
+int
+main()
+{
+  foo<>();
+  foo<1>();
+  foo<1, 2>();
+  // foo<1, -1>();
+}
--- variadic/sum.cc
+++ variadic/sum.cc
@@ -1,3 +1,6 @@
 #include <iostream>
 
-constexpt 
+int
+main()
+{
+}
