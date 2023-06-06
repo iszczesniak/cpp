4a2b56ff4c826a2ece7e1ce9da008dad97945ba0
iszczesniak <irek.szczesniak@gmail.com>
2023-06-06 09:21:25 +0200

    variadic

--- variadic/props.cc
+++ variadic/props.cc
@@ -1,17 +1,52 @@
-template <typename Edge, typename... Props>
-struct vertex: Props...
+#include <iostream>
+#include <string>
+
+// name --------------------------------------------------------------
+
+template <typename T>
+struct name
 {
-  vertex(Props &&... props): Props(std::forward<Props>(props))...
+  using name_type = T;
+  name_type m_name;
+
+  name(const name_type &name): m_name(name)
   {
   }
 
-  // constexpr auto operator <=> (const weight &) const = default;
+  constexpr auto operator <=> (const name &) const = default;
 };
 
-template <typename Weight>
+// The non-const getter.
+template <typename T>
+auto &
+get_name(name<T> &a)
+{
+  return a.m_name;
+}
+
+// The const getter.
+template <typename T>
+const auto &
+get_name(const name<T> &a)
+{
+  return a.m_name;
+}
+
+template <typename T>
+std::ostream &
+operator << (std::ostream &os, const name<T> &w)
+{
+  os << "name = " << w.m_name;
+
+  return os;
+}
+
+// weight ------------------------------------------------------------
+
+template <typename T>
 struct weight
 {
-  using weight_type = Weight;
+  using weight_type = T;
   weight_type m_weight;
 
   weight(const weight_type &weight): m_weight(weight)
@@ -22,31 +57,58 @@ struct weight
 };
 
 // The non-const getter.
-template <typename Weight>
+template <typename T>
 constexpr auto &
-get_weight(weight<Weight> &a)
+get_weight(weight<T> &a)
 {
   return a.m_weight;
 }
 
 // The const getter.
-template <typename Weight>
+template <typename T>
 constexpr const auto &
-get_weight(const weight<Weight> &a)
+get_weight(const weight<T> &a)
 {
   return a.m_weight;
 }
 
-template <typename Weight>
+template <typename T>
 std::ostream &
-operator << (std::ostream &os, const weight<Weight> &w)
+operator << (std::ostream &os, const weight<T> &w)
 {
   os << "weight = " << w.m_weight;
 
   return os;
 }
 
+// Type A can use properties. ----------------------------------------
+
+template <typename ...P>
+struct A: P...
+{
+  A(P && ...p): P(std::forward<P>(p))...
+    {
+    }
+
+  constexpr auto operator <=> (const A &) const = default;
+
+  std::ostream &
+    operator << (std::ostream &out) const
+  {
+    (out << ... << static_cast<P>(*this));
+    return out;
+  }
+};
+
+// The main function -------------------------------------------------
+
 int
 main()
 {
+  A<name<std::string>, weight<int>>
+    a(name<std::string>("Hello"), weight<int>(1));
+  get_weight(a) = 2;
+  get_name(a) = "World";
+  //  std::cout << a;
+  std::cout << (a < a);
 }
