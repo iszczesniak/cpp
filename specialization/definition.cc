dcbded90b2acddc527aca2c971946434b7e8d684
Irek Szcześniak <irek.szczesniak@gmail.com>
2023-10-31 10:48:21 +0100

    specialization: an difference between specialization and definition

---
+++ specialization/definition.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+// The primary template.
+template <class T>
+struct A
+{
+  void foo()
+  {
+    cout << __PRETTY_FUNCTION__ << endl;
+  }
+};
+
+// Specialization of the member function of the primary template.
+// Everything in A stays the same except our specialization.
+
+template<>
+void A<char>::foo()
+{
+  cout << "special = " << __PRETTY_FUNCTION__ << endl;
+}
+
+// The above specialization of a member function trigers the
+// instantiation of the primary template for T = char, so we cannot
+// now specialize the primary template for T = char.
+
+// If we want to specialize the primary template (that is commented
+// out below), we should comment out the specialization above.
+
+// Specialization of the primary template for T = char.
+// template <>
+// struct A<char>
+// {
+//   void foo();
+// };
+
+// This is the definition of a member function, not a template
+// specialization, so we cannot use 'template <>'.
+// void A<char>::foo()
+// {
+// }
+
+int main()
+{
+  A<int>().foo();
+  A<char>().foo();
+}
