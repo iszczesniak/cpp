f7ef2841192fb92721d71f1015ad03f868388dd9
iszczesniak <irek.szczesniak@gmail.com>
2024-03-03 12:09:15 +0100

    string

---
+++ categories/exercies/string.cc
@@ -0,0 +1,21 @@
+#include <string>
+
+using namespace std;
+
+string &operator++(string &s)
+{
+  return s;
+}
+
+string &operator++(string &s, int)
+{
+  return s;
+}
+
+int
+main()
+{
+  string s;
+  ++s++;
+  s++++;
+}
