34085bfdfcdbe1ef7bd5ec44475fb8b26d546ee5
iszczesniak <irek.szczesniak@gmail.com>
2024-03-08 10:45:16 +0100

    identity

--- categories/identity.cc
+++ categories/identity.cc
@@ -1,5 +1,3 @@
-#include <cassert>
-
 struct A
 {
   constexpr A() = default;
@@ -8,19 +6,23 @@ struct A
 int
 main()
 {
+  // Is the identity the same as the address?
+  
   const int i1 = 1;
   const int i2 = 1;
-  // Do i1 and i2 have the same identity?
-  // assert(&i1 == &i2);
+  static_assert(i1 == i2);
+  // Addresses of i1 and i2 differ.
+  static_assert(&i1 != &i2);
 
   constexpr int I1 = 1;
   constexpr int I2 = 1;
-  // Do I1 and I2 have the same identity?
-  // assert(&I1 == &I2);
+  static_assert(I1 == I2);
+  // Addresses of I1 and I2 differ.
+  static_assert(&I1 != &I2);
 
   constexpr A a1, a2;
-  // Do a1 and a2 have the same identity?
-  // assert(&a1 == &a2);
+  // Addresses of a1 and a2 differ.
+  static_assert(&a1 != &a2);
   
   // What's the identity anyway?
 }
