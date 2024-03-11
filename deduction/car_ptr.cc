39241588f1b8a361da9db8cda22982481c32096f
Irek Szcześniak <irek.szczesniak@gmail.com>
2024-03-11 09:59:22 +0100

    comments

--- deduction/car_ptr.cc
+++ deduction/car_ptr.cc
@@ -11,11 +11,9 @@ foo(A *a)
   for(auto &e: *a)
     cout << e << endl;
 
-  // I thought the following should not compile, but it does.  I hoped
-  // to get an error or at least a warning, because the compiler knows
-  // the function is instantiated for this type: int (*)[3]
-
-  // cout << a[3] << endl;
+  // Should the following compile for an array of three or less?  GCC
+  // gives no warning, but clang does.
+  cout << a[3] << endl;
 }
 
 void
