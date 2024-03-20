95e4adb25351e0cc1596e0ab2cd5d0f34b00cae6
Irek Szcześniak <irek.szczesniak@gmail.com>
2024-03-20 14:20:34 +0100

    comment

--- deduction/foo_ref.cc
+++ deduction/foo_ref.cc
@@ -25,4 +25,11 @@ main()
 {
   foo(hello);
   goo(hello);
+
+  // An expression of a pointer type is an rvalue, and so &hello is an
+  // rvalue.  Since it is of a non-const type, the constness cannot be
+  // deduced, and therefore the non-const reference parameter "F &"
+  // cannot be initialized with an rvalue.  It would compile if the
+  // foo parameter was a const reference.
+  // foo(&hello);
 }
