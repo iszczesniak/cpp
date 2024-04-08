98a0440ef8e37eccb57414af31794c0b95a21fda
iszczesniak <irek.szczesniak@gmail.com>
2024-04-08 08:56:42 +0200

    collapse

--- forwarding/collapse.cc
+++ forwarding/collapse.cc
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+// A reference does not have a top-level qualifier.
+
 int
 main()
 {
@@ -11,11 +13,43 @@ main()
   using r_type = int &&;
   using cr_type = const int &&;
 
-  // A reference to the all other reference types.
+  // A non-const lvalue reference to all other reference types.
+  // int & & -> int &
   static_assert(is_same_v<l_type &, int &>);
+  // const int & & -> const int &
   static_assert(is_same_v<cl_type &, const int &>);
+  // int && & -> int &
   static_assert(is_same_v<r_type &, int &>);
+  // const int && & -> const int &
   static_assert(is_same_v<cr_type &, const int &>);
 
-  // Implement for all possible compilations as above.
+  // A const lvalue reference to all other reference types.
+  // int & const & -> int &
+  static_assert(is_same_v<l_type const &, int &>);
+  // const int & const & -> const int &
+  static_assert(is_same_v<cl_type const &, const int &>);
+  // int && const & -> int &
+  static_assert(is_same_v<r_type const &, int &>);
+  // const int && const & -> const int &
+  static_assert(is_same_v<cr_type const &, const int &>);
+
+  // A non-const rvalue reference to all other reference types.
+  // int & && -> int &
+  static_assert(is_same_v<l_type &&, int &>);
+  // const int & && -> const int &
+  static_assert(is_same_v<cl_type &&, const int &>);
+  // int && && -> int &&
+  static_assert(is_same_v<r_type &&, int &&>);
+  // const int && && -> const int &&
+  static_assert(is_same_v<cr_type &&, const int &&>);
+
+  // A const rvalue reference to all other reference types.
+  // int & const && -> int &
+  static_assert(is_same_v<l_type const &&, int &>);
+  // const int & const && -> const int & 
+  static_assert(is_same_v<cl_type const &&, const int &>);
+  // int && const && -> int &&
+  static_assert(is_same_v<r_type const &&, int &&>);
+  // const int && const && -> const int &&
+  static_assert(is_same_v<cr_type const &&, const int &&>);
 }
