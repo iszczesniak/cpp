775ead680b90952acfcb901f066448cb8e7e56bf
iszczesniak <irek.szczesniak@gmail.com>
2024-04-09 09:15:53 +0200

    exercises

--- forwarding/exercises/base.cc
+++ forwarding/exercises/base.cc
@@ -1,13 +1,6 @@
 #include <iostream>
 #include <memory>
 
-template<typename A, typename T>
-auto
-my_make_unique(T &&t)
-{
-  return std::unique_ptr<A>(new A(std::forward<T>(t)));
-}
-
 struct A
 {
   A(std::string &)
@@ -21,10 +14,18 @@ struct A
   }
 };
 
+struct B: A
+{
+  template<typename T>
+  B(T &&t): A(std::forward<T>(t))
+  {
+  }
+};
+
 int
 main()
 {
   std::string a("World");
-  my_make_unique<A>(a);
-  my_make_unique<A>("Hello");
+  B{a};
+  B("Hello");
 }
