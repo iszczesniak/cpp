27d187bce74f18dd45e05581f06d97b9f2c9758d
iszczesniak <irek.szczesniak@gmail.com>
2024-04-16 09:22:10 +0200

    foo

--- deduction/foo_ref.cc
+++ deduction/foo_ref.cc
@@ -27,9 +27,22 @@ main()
   goo(hello);
 
   // An expression of a pointer type is an rvalue, and so &hello is an
-  // rvalue.  Since it is of a non-const type, the constness cannot be
-  // deduced, and therefore the non-const reference parameter "F &"
-  // cannot be initialized with an rvalue.  It would compile if the
-  // foo parameter was a const reference.
-  // foo(&hello);
+  // rvalue.  Since the hello function is of a non-const type (there
+  // is no such thing as a const non-member function), the constness
+  // cannot be deduced, and therefore the non-const reference
+  // parameter "F &" cannot be initialized with an rvalue.  It would
+  // compile if the foo parameter was a const reference.
+
+  foo(*hello);
+
+  // To continue, it makes sense that a non-const function cannot be
+  // declated const, because it has no object to work :
+
+  // void goo() const;
+
+  // But if so, then why does the following compile:
+  using fun_type = void() const;
+
+  // And the following doesn't:
+  fun_type *p = hello;
 }
