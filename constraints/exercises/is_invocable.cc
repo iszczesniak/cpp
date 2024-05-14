9acface94e2e395fb4e7a78cc08f23c07a06688d
iszczesniak <irek.szczesniak@gmail.com>
2024-05-14 09:38:15 +0200

    invocable

--- constraints/exercises/is_invocable.cc
+++ constraints/exercises/is_invocable.cc
@@ -5,8 +5,20 @@ struct A
   void operator()();
 };
 
+void foo(int);
+
 int
 main()
 {
+  // We pass only the invocable type, without the types of the
+  // arguments, so we expect to call the callable without the
+  // arguments.
   static_assert(std::is_invocable<A>::value);
+  static_assert(std::is_invocable_v<A>);
+
+  static_assert(!std::is_invocable<decltype(foo)>::value);
+  static_assert(!std::is_invocable_v<decltype(foo)>);
+
+  static_assert(std::is_invocable<decltype(foo), int>::value);
+  static_assert(std::is_invocable_v<decltype(foo), int>);
 }
