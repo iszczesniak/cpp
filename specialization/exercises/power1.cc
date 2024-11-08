cafb2267d20f6dd03bde9686c0b2aa58efd8734a
iszczesniak <irek.szczesniak@gmail.com>
2024-11-08 08:59:35 +0100

    specialization

---
+++ specialization/exercises/power1.cc
@@ -0,0 +1,35 @@
+#include <iostream>
+
+template <unsigned I, typename T>
+T
+power(T t)
+{
+  return t * power<I - 1>(t);
+}
+
+template <>
+double power<0>(double t)
+{
+  return 1;
+}
+
+template <>
+int power<0>(int t)
+{
+  return 1;
+}
+
+int
+main()
+{
+  // The value calculated at compile-time, nothing at run-time?
+  std::cout << power<2>(.2) << std::endl;
+
+  // We use the volatile qualifier to force evaluation at run-time.
+  volatile int d = 3;
+  // The assemby code uses the value of d without any calculation.
+  std::cout << power<1>(d) << std::endl;
+
+  // The assemby code just multiplies d by itself.
+  std::cout << power<2>(d) << std::endl;
+}
