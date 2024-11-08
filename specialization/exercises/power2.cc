5e2ea7dda2bcaab688f9d54f38d93db29da2a9d8
iszczesniak <irek.szczesniak@gmail.com>
2024-11-08 09:30:44 +0100

    specialization

--- specialization/exercises/power2.cc
+++ specialization/exercises/power2.cc
@@ -3,9 +3,27 @@
 template <typename T, T M, T N>
 T power()
 {
-  if constexpr (N)
-    return M * power<T, M, N - 1>();
+  return M * power<T, M, N - 1>();
+}
 
+// The problem defies a good solution with a template specialization:
+// for every pair of type and x, we have to provide a specialization.
+
+template <>
+int power<int, 2, 0>()
+{
+  return 1;
+}
+
+template <>
+unsigned long long power<unsigned long long, 2, 0>()
+{
+  return 1;
+}
+
+template <>
+unsigned long long power<unsigned long long, 3, 0>()
+{
   return 1;
 }
 
@@ -13,5 +31,6 @@ int
 main()
 {
   std::cout << power<int, 2, 10>() << std::endl;
-  std::cout << power<unsigned long long, 2, 64>() << std::endl;
+  std::cout << power<unsigned long long, 2, 32>() << std::endl;
+  std::cout << power<unsigned long long, 3, 32>() << std::endl;
 }
