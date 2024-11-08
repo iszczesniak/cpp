494c8803a7c266131e6832a3385fc8c1c6c22ae2
iszczesniak <irek.szczesniak@gmail.com>
2024-11-08 09:35:10 +0100

    templates

--- templates/exercises/power2.cc
+++ templates/exercises/power2.cc
@@ -13,7 +13,7 @@ int
 main()
 {
   std::cout << power<int, 2, 10>() << std::endl;
-  // Take a look at the symbol table of the executable.  There are 65
+  // Take a look at the symbol table of the executable.  There are 33
   // template functions generated!
-  std::cout << power<unsigned long long, 2, 64>() << std::endl;
+  std::cout << power<unsigned long long, 2, 32>() << std::endl;
 }
