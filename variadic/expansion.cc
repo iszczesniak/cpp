b231cce0fe8a902bf782420606843ada4f67f44c
iszczesniak <irek.szczesniak@gmail.com>
2024-11-15 08:36:41 +0100

    variadic

--- variadic/expansion.cc
+++ variadic/expansion.cc
@@ -19,8 +19,8 @@ main()
 {
   // Expands to a structure type that does not have a base class.
   B<>();
-  // Expands to a structure type derived from A and string.
-  B<A>();
   // Expands to a structure type that has the base class A.
+  B<A>();
+  // Expands to a structure type derived from A and string.
   B<A, std::string>();
 }
