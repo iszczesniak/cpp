e9c4b829726d8f28d0c155b8b1397c76aa9a4b30
Irek Szczesniak <iszczesniak@home.my.domain>
2024-11-16 12:29:40 +0100

    iterate

--- containers/iterate_new.cc
+++ containers/iterate_new.cc
@@ -2,38 +2,30 @@
 #include <vector>
 #include <utility>
 
+using namespace std;
+
 int main()
 {
-  std::vector<int> v = {1, 2, 3};
-
-  for(auto &e: v)
-    std::cout << e << std::endl;
+  vector v = {1, 2, 3};
 
   // We can modify the elements, because we use them through a
   // non-const reference of type: auto &
   for(auto &e: v)
-    std::cout << ++e << std::endl;
+    cout << ++e << endl;
 
   // Here we iterate with a const reference.
   for(const auto &e: v)
-    std::cout << e << std::endl;
+    cout << e << endl;
 
   // This would not compile, because we refer to the elements through
   // a const reference: const auto &
 
   // for(const auto &e: v)
-  //   std::cout << ++e << std::endl;
+  //   cout << ++e << endl;
 
   // We could also iterate through a non-const container and reference
   // the elements with a const reference, becasue we refer to the
   // non-const container with a const reference.
-
-  for(auto &e: std::as_const(v))
-    std::cout << e << std::endl;
-  
-  // This would not compile, because we're trying to modify elements
-  // of container referred to with a const reference.
-
-  // for(auto &e: std::as_const(v))
-  //   std::cout << ++e << std::endl;
+  for(auto &e: as_const(v))
+    cout << e << endl;
 }
