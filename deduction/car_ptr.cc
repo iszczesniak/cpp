031388994d752d2aaca44adf826a078e1cc9eb98
Irek Szczesniak <iszczesniak@home.my.domain>
2024-11-16 12:52:16 +0100

    simplifying the examples for deduction for C-style arrays

--- deduction/car_ptr.cc
+++ deduction/car_ptr.cc
@@ -1,28 +1,16 @@
 #include <iostream>
 
-using namespace std;
-
 template <typename A>
 void
 foo(A *a)
 {
   std::cout << __PRETTY_FUNCTION__ << std::endl;
-
-  for(auto &e: *a)
-    cout << e << endl;
-
-  // Should the following compile for an array of three or less?  GCC
-  // gives no warning, but clang does.
-  cout << a[3] << endl;
 }
 
 void
 goo(int (*a)[3])
 {
-  for(auto &e: *a)
-    cout << e << endl;
-
-  // cout << a[3] << endl;
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
 int
--- deduction/car_val.cc
+++ deduction/car_val.cc
@@ -1,51 +1,16 @@
 #include <iostream>
 
-using namespace std;
-
-template <typename A>
-void
-foo(A a)
-{
-  std::cout << __PRETTY_FUNCTION__ << std::endl;
-
-  // The following could not compile: when instantiated in the
-  // example, "a" cannot be used in the foreach loop, because it's of
-  // type "int *".
-  //
-  // for(auto &e: *a)
-  //   cout << e << endl;
-
-  cout << a[0] << endl;
-  cout << a[1] << endl;
-  cout << a[2] << endl;
-}
-
-// This parameter type doesn't mean passing an array by value.  It
-// just means: A *a.
 template <typename A>
 void
 foo(A a[])
 {
-  std::cout << "A a[]: " << __PRETTY_FUNCTION__ << std::endl;
-
-  // This compiles without an error or a warning, because the compiler
-  // has no clue we're using a table of three elements.
-  cout << a[3] << endl;
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
 void
-goo(int *a)
+goo(int a[])
 {
-  // for(auto &e: *a)
-  //   cout << e << endl;
-
-  cout << a[0] << endl;
-  cout << a[1] << endl;
-  cout << a[2] << endl;
-
-  // This compiles without an error or a warning, because the compiler
-  // has no clue we're using a table of three elements.
-  cout << a[3] << endl;
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
 }
 
 int
