b0faad0249176bfe86df3df5ef356e0e154957f2
Irek Szczesniak <iszczesniak@home.my.domain>
2024-11-16 12:10:22 +0100

    itetating

--- containers/iterate_old.cc
+++ containers/iterate_old.cc
@@ -1,56 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <utility>
+
+using namespace std;
 
 int main()
 {
-  std::vector<int> v;
-  v.push_back(1);
-  v.push_back(2);
-  v.push_back(3);  
+  vector v = {1, 2, 3};
 
-  // The legacy way of iterating.
-  for(std::vector<int>::iterator i = v.begin();
-      i != v.end(); ++i)
-    std::cout << *i << std::endl;
-
-  // We can modify the elements, because we're using a non-const
-  // iterator.
-  for(std::vector<int>::iterator i = v.begin();
-      i != v.end(); ++i)
-    std::cout << (*i)++ << std::endl;
+  // The legacy way of iterating.  We can modify the elements, because
+  // we're using a non-const iterator.
+  for(vector<int>::iterator i = v.begin(); i != v.end(); ++i)
+    cout << (*i)++ << endl;
 
   // That would not compile, because we're trying to modifying the
   // elements that we access through a const iterator.
-
-  // for(std::vector<int>::const_iterator i = v.begin();
-  //     i != v.end(); ++i)
-  //   std::cout << (*i)++ << std::endl;
+  // for(vector<int>::const_iterator i = v.begin(); i != v.end(); ++i)
+  //   cout << (*i)++ << endl;
 
   // We can use the 'auto' type -- the preferred way, because it's
   // less error-prone.  We let the compiler deduce the iterator type.
   for(auto i = v.begin(); i != v.end(); ++i)
-    std::cout << (*i)++ << std::endl;
+    cout << (*i)++ << endl;
 
   // We're using const iterators here.
   for(auto i = v.cbegin(); i != v.cend(); ++i)
-    std::cout << *i << std::endl;
+    cout << *i << endl;
 
-  // That would not compile, because we're trying to modifying the
-  // elements that we access through a const iterator.
-
-  // for(auto i = v.cbegin(); i != v.cend(); ++i)
-  //   std::cout << (*i)++ << std::endl;
-
-  // We're using const iterators here.
-  for(auto i = std::as_const(v).begin();
-      i != std::as_const(v).end(); ++i)
-    std::cout << *i << std::endl;
-
-  // That would not compile, because we're trying to modifying the
-  // elements that we access through a const iterator.
-
-  // for(auto i = std::as_const(v).begin();
-  //     i != std::as_const(v).end(); ++i)
-  //   std::cout << (*i)++ << std::endl;
+  // We're using const iterators here as well.
+  for(auto i = begin(as_const(v)); i != end(as_const(v)); ++i)
+    cout << *i << endl;
 }
