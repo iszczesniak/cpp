1551f8c82b0f3324ac3ba4206208ba3efd219be4
Irek Szcześniak <iszczesniak@debian>
2025-02-05 08:42:50 +0100

    exercise

---
+++ templates/exercises/add.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+template <typename Param, typename... Params>
+auto add(const Param &param, const Params &... params)
+{
+  if constexpr (sizeof...(params))
+    return param + fun(params...);
+  else
+    return param;
+}
+
+int
+main()
+{
+  std::cout << add(1, 2) << std::endl;
+  std::cout << add(1, 2.3) << std::endl;
+  std::cout << add(std::string("Hello "), "World!") << std::endl;
+}
