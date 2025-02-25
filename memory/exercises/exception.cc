6e6456089ecbd4cd302f416bb0e5d587b8952fab
Irek Szcześniak <iszczesniak@debian>
2025-02-25 12:38:25 +0100

    exe

--- memory/exercises/exception.cc
+++ memory/exercises/exception.cc
@@ -1,13 +1,16 @@
 struct A
 {
   A() = default;
-  // Explicitly deletes the move-ctor, and implicitly the copy-ctor.
-  A(A &&) = delete;
+  // Explicitly delete the copy-ctor (the move-ctor is implicitly left
+  // undeclared).
+  A(const A &) = delete;
 };
 
 int
 main()
 {
+  // The constructor elision takes place, because the code compiles.
+  // The thrown object is constructed in place of variable a.
   try
     {
       throw A();
