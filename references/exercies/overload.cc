bfb4bdd61258a316838ed3a486f34bb30172df94
Irek Szczesniak <iszczesniak@home.my.domain>
2025-03-16 14:50:19 +0100

    references

---
+++ references/exercies/overload.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <utility>
+
+struct A
+{
+};
+
+void operator++(A &a)
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+void operator++(A &&a)
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+int
+main()
+{
+  A a;
+  ++a;
+  ++A();
+}
