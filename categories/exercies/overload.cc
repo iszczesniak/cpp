21677ec35860b0e33715c40a191035402a812d65
Irek Szczesniak <iszczesniak@home.my.domain>
2025-03-16 14:11:46 +0100

    categories

--- categories/exercies/overload.cc
+++ categories/exercies/overload.cc
@@ -3,35 +3,21 @@
 
 struct A
 {
-  A &operator++() &
+  void foo() &
   {
     std::cout << __PRETTY_FUNCTION__ << std::endl;
-    return *this;
   }
 
-  A &&operator++() &&
+  void foo() &&
   {
     std::cout << __PRETTY_FUNCTION__ << std::endl;
-    return std::move(*this);
   }
 };
-
-// A &operator++(A &a)
-// {
-//   std::cout << __PRETTY_FUNCTION__ << std::endl;
-//   return a;
-// }
-
-// A &&operator++(A &&a)
-// {
-//   std::cout << __PRETTY_FUNCTION__ << std::endl;
-//   return std::move(a);
-// }
 
 int
 main()
 {
-  ++++A{};
   A a;
-  ++++a;
+  a.foo();
+  A().foo();
 }
