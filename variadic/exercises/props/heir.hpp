b5ddfa5100e776b070134d92df8908c7126d8d1d
Irek Szcześniak <iszczesniak@debian>
2025-03-17 13:41:25 +0100

    variadic

---
+++ variadic/exercises/props/heir.hpp
@@ -0,0 +1,26 @@
+#ifndef HEIR_HPP
+#define HEIR_HPP
+
+#include <iostream>
+#include <utility>
+
+// Type A can use properties. ----------------------------------------
+
+template <typename... P>
+struct heir: P...
+{
+  heir(P &&... p): P(std::forward<P>(p))...
+    {
+    }
+};
+
+template <typename P1, typename... Ps>
+std::ostream &
+operator << (std::ostream &out, const heir<P1, Ps...> &a)
+{
+  out << static_cast<const P1 &>(a);
+  (... , (out << ", " << static_cast<const Ps &>(a)));
+  return out;
+}
+
+#endif // HEIR_HPP
