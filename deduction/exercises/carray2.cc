c939f57236321a06800556ccab1cdaaaf88fcfb0
Irek Szczesniak <iszczesniak@home.my.domain>
2025-04-18 15:25:38 +0200

    carray

---
+++ deduction/exercises/carray2.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iterator>
+
+template <typename T>
+void foo(T &t)
+{
+  for(std::size_t i = 0; i < std::size(t); ++i)
+    std::cout << t[i];
+}
+
+int main()
+{
+  int t[] = {1, 2, 3};
+  foo(t);
+}
