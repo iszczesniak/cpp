8e224c8e843d598e16ab37a5c0f8442210ac5276
Irek Szczesniak <iszczesniak@puffy.my.domain>
2025-05-12 09:03:01 +0200

    comparison1: reversed

--- comparison1/reversed.cc
+++ comparison1/reversed.cc
@@ -1,3 +1,5 @@
+#include <cassert>
+
 struct A
 {
   bool operator == (const A &) const = default;
@@ -5,10 +7,15 @@ struct A
 
 struct B: A
 {
-  using A::operator ==;
+  // using A::operator ==;
+
+  // bool operator == (const B &b) const
+  // {
+  //   return static_cast<const A &>(*this) == static_cast<const A &>(b);
+  // }
 };
 
 int main()
 {
-  B() == B();
+  assert(B() == B());
 }
