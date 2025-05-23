285d8cf6ed9d3314a38f8e0377599ce254ee2317
Irek Szcześniak <irek.szczesniak@gmail.com>
2025-05-23 13:48:26 +0200

    reference

--- overloading1/reference.cpp
+++ overloading1/reference.cpp
@@ -1,7 +1,10 @@
-void foo(const int &);
-void foo(int &&);
+void foo(const int &); // #1
+void foo(int &&); // #2
 
 int main()
 {
+  // Calls #2, because initializing an rvalue reference with an rvalue
+  // is better than initializing an lvalue reference
+  // [over.ics.rank#3.2.3].
   foo(1);
 }
