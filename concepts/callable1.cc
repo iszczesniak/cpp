c0363c507004e045aa0e44dddf5be80afcf4a2b0
Irek Szcześniak <irek.szczesniak@gmail.com>
2025-05-23 14:22:03 +0200

    callable

--- concepts/callable1.cc
+++ concepts/callable1.cc
@@ -1,21 +1,30 @@
 template <typename F, typename... P>
 concept Callable = requires (F f, P... p)
-  {
-    f(p...);
-  };
+{
+  f(p...);
+};
+
+void f(int, bool);
 
 void g();
 void g(int);
-void g1(int, bool);
 
 int main()
 {
+  // Here we pass a function type 'void()', and make sure we can call
+  // it with no argument.
   static_assert(Callable<void()>);
+
+  // Can we call f with an int and a bool?
+  static_assert(Callable<decltype(f), int, bool>);
+
+  // We cannot just say 'decltype(f)', because the compiler wouldn't
+  // know which overload we mean, and thefore we need to static_cast.
   static_assert(Callable<decltype(static_cast<void(*)()>(g))>);
   static_assert(Callable<decltype(static_cast<void(*)(int)>(g)), int>);
-  static_assert(Callable<decltype(g1), int, bool>);
 
-  // The declaration below overshadows this overload: void g();
+  // The declaration below overshadows the 'void g()' overload, so now
+  // we can say 'decltype(g)'.
   void g(int);
   static_assert(Callable<decltype(g), int>);
 }
