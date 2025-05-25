93235c63b3ad0cedd921c3a246f96684e0da266b
Irek Szczesniak <iszczesniak@dell.my.domain>
2025-05-25 08:26:05 +0200

    overloading1

--- overloading1/partial2.cc
+++ overloading1/partial2.cc
@@ -1,72 +1,24 @@
 #include <iostream>
 
-// We have two primary function templates.  The first has a single
-// template parameter T, the other has two parameters T1 and T2.  How
-// does the compiler choose the right template?  Is this overload
-// resolution?
-//
-// f(1, 1);
-//
-// f(true, 1);
+using namespace std;
 
-// Template #1
 template <typename T>
-void foo(T t1, T t2)
+void foo(T &, const T&)
 {
-  std::cout << "Template with one parameter.\n";
+  cout << __PRETTY_FUNCTION__ << endl;
 }
 
-// Template #2
-template <typename T1, typename T2>
-void foo(T1 t1, T2 t2)
+template <typename T>
+void foo(const T &, T &)
 {
-  std::cout << "Template with two parameters.\n";
+  cout << __PRETTY_FUNCTION__ << endl;
 }
 
 int main()
 {
-  // Step 1: template #1 is transformed into this function type:
-  //
-  // void (U, U) // function type #1
+  int x = 1;
+  foo(x, x);
 
-  // We got U, because [temp.func.order] says "for each (...)
-  // parameter (...) synthesize a unique type".  Template 1 has one
-  // parameter T only for which we make up ("synthesize") type U.
-  //
-  // The above type is the function type transformed from the argument
-  // template.
-  //
-  // Template #2 is called the parameter template, because for that
-  // template we try to deduce the arguments based on the template #1.
-  // Template #1 is called the argument template, because it's given,
-  // and doesn't change.  Specifically, the deduction takes into
-  // account as the argument the function type #1.
-  //
-  // Deduced arguments for template #2: T1 = U1, T2 = U2.  Success.
-
-  // Step 2: template #2 is transformed into this function type:
-  //
-  // void (U1, U2)
-  //
-  // We got unique types U1, and U2, because [temp.func.order] says
-  // "for each (...) parameter (...) synthesize a unique type".
-  // Template 2 has two parameters T1 and T2 for which we make up
-  // types U1 and U2.
-  //
-  // We cannot deduce arguments for template #1, because T cannot be
-  // both U1, and U2.  Failure.
-
-  // Conclusion
-  // ----------
-  //
-  // Template #1 is used, because it is more specialized than template
-  // #2: calls to #1 are a subset of calls to #2, i.e., whenever
-  // template #1 can be used, template #2 can be used.  We can say
-  // that template #1 implies template #2: if template #1 can be used,
-  // then template #2 could be used too, but not the other way around.
-  
-  foo(1, 1);
-
-  // The second case.
-  foo(true, 1);
+  const int cx = 1;
+  foo(cx, cx);
 }
