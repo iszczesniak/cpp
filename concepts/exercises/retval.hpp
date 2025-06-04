5012f6c3d2c4e5a52142092bc96900e12fff8d99
Irek Szczesniak <iszczesniak@dell.my.domain>
2025-06-04 22:39:55 +0200

    retval

--- concepts/exercises/retval.hpp
+++ concepts/exercises/retval.hpp
@@ -7,6 +7,9 @@ template <typename F, typename T>
 concept retval = requires (F f)
 {
   {f()} -> std::same_as<T>;
+
+  // The following does the same:
+  // std::same_as<decltype(f()), T>;
 };
 
 #endif // RETVAL_HPP
