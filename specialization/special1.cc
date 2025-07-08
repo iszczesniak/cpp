c27f5830df583f226fa6c2dcecbf0e86d07e2ff5
Irek Szczesniak <iszczesniak@dell.my.domain>
2025-07-08 22:36:53 +0200

    comments

--- specialization/special1.cc
+++ specialization/special1.cc
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-// Overload A
+// Template A
 template <typename A1>
 void foo(A1 a1)
 {
   cout << __PRETTY_FUNCTION__ << endl;
 }
 
-// Overload B
+// Template B
 template <typename B1>
 void foo(B1 *b1)
 {
@@ -19,11 +19,11 @@ void foo(B1 *b1)
 int main()
 {
   int x = 1;
-  // Call #1: only overload A is a candidate.  The template function
+  // Call #1: only template A is a candidate.  The template function
   // called: void foo(int).
   foo(x);
 
-  // Call #2: overloads A and B are the candidates.  The template
+  // Call #2: templates A and B are the candidates.  The template
   // function called: void foo(int *).
   foo(&x);
 }
