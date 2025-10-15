a4f13f62a4a6b3d1d5763f1194c4e8caf9d39789
Irek Szcześniak <irek.szczesniak@gmail.com>
2025-10-15 13:24:10 +0200

    complex

--- variadic/complex.cc
+++ variadic/complex.cc
@@ -14,6 +14,10 @@ main()
 {
   print(5, "10", std::string("15"));
   std::cout << std::endl;
+
   // What's this?
   1, void();
+  // Well, it's needed here:
+  print(1);
+  std::cout << std::endl;  
 }
