0b75912d7075751084a0b31016db46cd130eb2aa
Irek Szcześniak <iszczesniak@debian>
2025-11-18 12:49:08 +0100

    comments

--- deduction/foo_ref.cc
+++ deduction/foo_ref.cc
@@ -26,12 +26,18 @@ main()
   foo(hello);
   goo(hello);
 
-  // An expression of a pointer type is an rvalue, and so &hello is an
-  // rvalue.  Since the hello function is of a non-const type (there
-  // is no such thing as a const non-member function), the constness
-  // cannot be deduced, and therefore the non-const reference
-  // parameter "F &" cannot be initialized with an rvalue.  It would
-  // compile if the foo parameter was a const reference.
+  // An expression of a pointer type is an rvalue, and so "&hello"
+  // below is an rvalue.  Since the hello function is of a non-const
+  // type (there is no such thing as a const non-member function), the
+  // constness cannot be deduced, and therefore the non-const
+  // reference parameter "F &" cannot be initialized with an rvalue.
+  // It would compile if the foo parameter was a const reference.
+  
+  // foo(&hello);
 
+  // What's that!?
   foo(*hello);
+  // It's the same as the following, because in "*hello", "hello"
+  // decays into "&hello".
+  foo(*&hello);
 }
