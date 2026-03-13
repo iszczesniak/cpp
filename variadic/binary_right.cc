83107750ff9ee2e1a90e520c2ef950ca1a35c36c
Irek Szcześniak <iszczesniak@debian>
2026-03-13 09:40:46 +0100

    binary fold expression

--- variadic/binary_right.cc
+++ variadic/binary_right.cc
@@ -12,6 +12,12 @@ void right(R &r, T &... p)
   (p += ... += r);
 }
 
+template <typename... T, typename R>
+void right_right(T &... p, R &r)
+{
+  (p += ... += r);
+}
+
 int main()
 {
   int x = 1, y = 2, z = 3;
@@ -24,8 +30,21 @@ int main()
   // right associativity, and so we need the right fold expression.
 
   // left(x, y, z);
-  right(x, y, z);
 
+  // This seems to be the natural order: x, y, z, but x becomes the
+  // initializing expression, and so this call works wrong.
+  // right(x, y, z);
+
+  // The initializing expression has to be passed first.
+  right(z, x, y);
+
+  // If we wanted to pass the arguments in the natural order: x, y, z,
+  // then the last function parameter should accept the initializing
+  // expression, like the function right_right does.  Unfortunately,
+  // it doesn't compile (with the newest GCC and clang on March 13,
+  // 2026), because the parameter pack p gobbles up all arguments,
+  // leaving none for the initialization of parameter r.
+  
   assert(x == 6);
   assert(y == 5);
   assert(z == 3);
