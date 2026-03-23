04ee63650dfadfa9f915c69a07e0c6b8d390bc6c
Irek Szcześniak <iszczesniak@debian>
2026-03-23 11:09:19 +0100

    memory

--- memory/args.cc
+++ memory/args.cc
@@ -1,28 +1,4 @@
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-struct A
-{
-  string m_name;
-
-  A(const string &name): m_name(name)
-  {
-    cout << "ctor: " << m_name << endl;
-  }
-
-  A(const A &a)
-  {
-    cout << "copy-ctor: " << a.m_name << endl;
-    m_name = a.m_name + " copy";
-  }
-
-  void hello() const
-  {
-    cout << "Hello from " << m_name << endl;
-  }
-};
+#include "A.hpp"
 
 void foo(A a)
 {
