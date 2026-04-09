d2512b936b2180a019d45e0cf1a06a6c775b6376
Irek Szcześniak <irek.szczesniak@gmail.com>
2026-04-09 15:51:21 +0200

    specialization

---
+++ specialization/deduction.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+template <typename T>
+void f(void (&)(const T &t))
+{
+  std::cout << __PRETTY_FUNCTION__ << std::endl;
+}
+
+void g1(const int &)
+{
+}
+
+void g2(int)
+{
+}
+
+int
+main()
+{
+  f(g1);
+  f(g2);
+}
