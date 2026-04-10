01bb60ec33cf3e9a6a35544baf793cf0322f77a5
Irek Szcześniak <iszczesniak@debian>
2026-04-10 10:38:55 +0200

    explicit

--- specialization/explicit.cc
+++ specialization/explicit.cc
@@ -1,35 +1,29 @@
+#include <array>
 #include <iostream>
-
-template <typename T>
-struct C
-{
-};
 
 // A declaration of the primary function template.
 template <typename T>
 void foo(const T &t);
 
-// Error: a partial specialization of a function template is not
-// allowed.
-
-// template <typename T>
-// void foo<C<T>>(const C<T> &)
+// Error: a partial specialization of a function template not allowed.
+// template <typename T, std::size_t I>
+// void foo<std::array<T, I>>(const std::array<T, I> &)
 // {
-//   std::cout << __PRETTY_FUNCTION__ << ": ";
-//   std::cout << "C<T>" << std::endl;
+//   std::cout << "A template specialization: "
+//             << __PRETTY_FUNCTION__ << std::endl;
 // }
 
-// This is another primary template.  It's not a specialization.
-template <typename T>
-void foo(const C<T> &)
+// This is another primary template.  It's not a specialization.  How
+// does it differ from the specialization above?
+template <typename T, std::size_t I>
+void foo(const std::array<T, I> &)
 {
-  std::cout << __PRETTY_FUNCTION__ << ": ";
-  std::cout << "C<T>" << std::endl;
+  std::cout << "A template overload: "
+            << __PRETTY_FUNCTION__ << std::endl;
 }
 
 int
 main()
 {
-  // foo(1);
-  foo(C<int>());
+  foo(std::array{1, 2, 3});
 }
