6d0a95ca1997af688500717b6aa306267db01933
Irek Szczesniak <iszczesniak@dell.my.domain>
2026-04-28 14:22:02 +0000

    lib

--- lib/A.cpp
+++ lib/A.cpp
@@ -8,16 +8,14 @@ A::A(const string &name): m_name(name)
   cout << "ctor: " << m_name << endl;
 }
 
-A::A(const A &a)
+A::A(const A &a): m_name(a.m_name + " copied")
 {
-  m_name = a.m_name + " copied";
-  cout << "copy-ctor: " << a.m_name << endl;
+  cout << "copy-ctor: " << m_name << endl;
 }
 
-A::A(A &&a)
+A::A(A &&a): m_name(a.m_name + " moved")
 {
-  m_name = a.m_name + " moved";
-  cout << "move-ctor: " << a.m_name << endl;
+  cout << "move-ctor: " << m_name << endl;
 }
 
 void A::hello() const
